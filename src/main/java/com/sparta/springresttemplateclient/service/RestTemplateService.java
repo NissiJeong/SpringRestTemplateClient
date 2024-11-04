aa8869e2fae5e57eaedb7fdc2622777d0bbf37b6
NissyJeong <jnissi92@gmail.com>
2024-11-04 11:54:02 +0900

    resttemplate get

--- src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -2,20 +2,64 @@ package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 public class RestTemplateService {
 
+    private final RestTemplate restTemplate;
+
+    public RestTemplateService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
     public ItemDto getCallObject(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-obj")
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        // get 방식으로 해당 url 의 서버에 요청을 함.
+        // 우리가 직접 jackson 을 사용하지 않아도 두번째 파라미터로 넘긴 entity 그대로 넘어온다.
+        ResponseEntity<ItemDto> responseEntity = restTemplate.getForEntity(uri, ItemDto.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
 
     public List<ItemDto> getCallList() {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/get-call-list")
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+        log.info("Body = " + responseEntity.getBody());
+
+        return fromJSONtoItems(responseEntity.getBody());
     }
 
     public ItemDto postCall(String query) {
@@ -24,5 +68,18 @@ public class RestTemplateService {
 
     public List<ItemDto> exchangeCall(String token) {
         return null;
+    }
+
+    public List<ItemDto> fromJSONtoItems(String responseEntity) {
+        JSONObject jsonObject = new JSONObject(responseEntity);
+        JSONArray items  = jsonObject.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Object item : items) {
+            ItemDto itemDto = new ItemDto((JSONObject) item);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
     }
 }
